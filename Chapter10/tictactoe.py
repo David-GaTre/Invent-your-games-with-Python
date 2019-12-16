832158ca34aeb2424377edee7a80ddcae88e8d60
David Garcia <gtdavid2013@gmail.com>
2019-12-15 22:25:22 -0600

    Added Chapter 10

--- Chapter10/tictactoe.py
+++ Chapter10/tictactoe.py
@@ -1,9 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Dec  9 09:49:02 2019
-
-@author: gtdav
-"""
 import random
 
 def drawBoard(board):
@@ -14,9 +8,168 @@ def drawBoard(board):
 	print(board[4] + '|' + board[5] + '|' + board[6])
 	print('-+-+-')
 	print(board[1] + '|' + board[2] + '|' + board[3])
-	
+
 def inputPlayerLetter():
 	# Lets the player type which letter they want to be.
 	# Return a list with the player's letter as the first
 	# item and the computer's letter as the second.
 	letter = ''
+	while not (letter == 'X' or letter == 'O'):
+		print('Do you want to be X or O?')
+		letter = input().upper()
+	# The first element in the list is the player's letter; the second is the
+	# computer's letter.
+	if letter == 'X':
+		return ['X','O']
+	else:
+		return ['O','X']
+
+def whoGoesFirst():
+	# Randomly choose which player goes first.
+	if random.randint(0, 1) == 0:
+		return 'computer'
+	else:
+		return 'player'
+
+def makeMove(board, letter, move):
+	board[move] = letter
+
+def isWinner(bo, le):
+	# Given a board and a player's letter, this function returns True if that
+	# player has won.
+	# We use "bo" instead of "board" and "le" instead of "letter" so we don't
+	# have to type as much.
+	return ((bo[7] == le and bo[8] == le and bo[9] == le) or # Top
+	(bo[4] == le and bo[5] == le and bo[6] == le) or # Middle
+	(bo[1] == le and bo[2] == le and bo[3] == le) or # Bot
+	(bo[7] == le and bo[4] == le and bo[1] == le) or # Left
+	(bo[8] == le and bo[5] == le and bo[2] == le) or # Down the middle
+	(bo[9] == le and bo[6] == le and bo[3] == le) or # Right
+	(bo[7] == le and bo[5] == le and bo[3] == le) or # Diagonal
+	(bo[9] == le and bo[5] == le and bo[1] == le)) # Diagonal
+
+def getBoardCopy(board):
+	# Make a copy of the board list and return it
+	boardCopy = []
+	for i in board:
+		boardCopy.append(i)
+	return boardCopy
+
+def isSpaceFree(board, move):
+	# Return True if the passed move is free on the passed board.
+	return board[move] == ' '
+
+def getPlayerMove(board):
+	# Let the player enter their move.
+	move = ' '
+	while move not in '1 2 3 4 5 6 7 8 9'.split() or not isSpaceFree(board, int(move)):
+		print('What is your next move? (1-9)')
+		move = input()
+	return int(move)
+
+def chooseRandomMoveFromList(board, movesList):
+	# Returns a valid move from the passed list on the passed board.
+	# Returns None if there is no valid move.
+	possibleMoves = []
+	for i in movesList:
+		if isSpaceFree(board, i):
+			possibleMoves.append(i)
+
+	if len(possibleMoves) != 0:
+		return random.choice(possibleMoves)
+	else:
+		return None
+
+def getComputerMove(board, computerLetter):
+	# Given a board and the computer's letter, determine where to move and return that move.
+	if computerLetter == 'X':
+		playerLetter = 'O'
+	else:
+		playerLetter = 'X'
+
+	# Here is the algorithm for our Tic-Tac-Toe AI:
+	# Check if we can win.
+	for i in range(1, 10):
+		boardCopy = getBoardCopy(board)
+		if isSpaceFree(boardCopy, i):
+			makeMove(boardCopy, computerLetter, i)
+			if isWinner(boardCopy, computerLetter):
+				return i
+
+	#Check if the player could win and block them
+	for i in range(1, 10):
+		boardCopy = getBoardCopy(board)
+		if isSpaceFree(boardCopy, i):
+			makeMove(boardCopy, playerLetter, i)
+			if isWinner(boardCopy, playerLetter):
+				return i
+
+	# Try to take one of the corners if free
+	move = chooseRandomMoveFromList(board, [1, 3, 7, 9])
+	if move != None:
+		return move
+
+	# Try to take the center if free.
+	if isSpaceFree(board, 5):
+		return 5
+
+	# Move on one of the sides.
+	return chooseRandomMoveFromList(board, [2, 4, 6, 8])
+
+def isBoardFull(board):
+	# Return True if every space on the board has been taken. Otherwise, False.
+	for i in range(1, 10):
+		if isSpaceFree(board, i):
+			return False
+	return True
+
+# Main
+print('Let\'s play SOME FUCKING TIC-TAC-TOE')
+
+while True:
+	# Reset the board
+	theBoard = [' '] * 10
+	playerLetter, computerLetter = inputPlayerLetter()
+	turn = whoGoesFirst()
+	print('The ' + turn + ' will go first.')
+	gameIsPlaying = True
+
+	while gameIsPlaying:
+		if turn == 'player':
+			# Player's turn
+			drawBoard(theBoard)
+			move = getPlayerMove(theBoard)
+			makeMove(theBoard, playerLetter, move)
+
+			if isWinner(theBoard, playerLetter):
+				drawBoard(theBoard)
+				print('Con madre, ganasteS')
+				gameIsPlaying = False
+			else:
+				if isBoardFull(theBoard):
+					drawBoard(theBoard)
+					print('Empate')
+					break
+				else:
+					turn = 'computer'
+
+		else:
+			# Computer's turn
+			move = getComputerMove(theBoard, computerLetter)
+			makeMove(theBoard, computerLetter, move)
+
+			if isWinner(theBoard, computerLetter):
+				drawBoard(theBoard)
+				print('Te gano la compu. Chale.')
+				gameIsPlaying = False
+			else:
+				if isBoardFull(theBoard):
+					drawBoard(theBoard)
+					print('Empate')
+					break
+				else:
+					turn = 'player'
+
+	print('Do you want to play again? (yes or no)')
+	if not input().lower().startswith('y'):
+		break
---
+++ Chapter11/bagels.py
@@ -0,0 +1,34 @@
+import random
+
+NUM_DIGITS = 3
+MAX_GUESS = 10
+
+def getSecretNum():
+    # Returns a string of unique random digits that is NUM_DIGITS long.
+    numbers = list(range(10))
+    random.shuffle(numbers)
+    secretNum = ''
+    for i in range(NUM_DIGITS):
+        secretNum += str(numbers[i])
+    return secretNum
+
+def getClues(guess, secretNum):
+    # Returns a string with the Pico, Fermi, & Bagels clues to the user.
+    if guess == secretNum:
+        return 'You got it!'
+
+    clues = []
+    for i in range(len(guess)):
+        if guess[i] == secretNum[i]:
+            clues.append('Fermi')
+        elif guess[i] in secretNum:
+            clues.append('Pico')
+    if len(clues) == 0:
+        return 'Bagels'
+
+    clues.sort()
+    return ' '.join(clues)
+
+def isOnlyDigits(num):
+    # Returns True if num is a string of only digits. Otherwise, False.
+    if num == '':
